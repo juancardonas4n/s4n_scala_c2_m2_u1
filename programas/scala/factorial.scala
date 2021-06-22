7997682d8871f9575a5b9ac57c47beda70fe1a22
Juan Francisco Cardona Mc'Cormick <juancardona@seven4n.com>
2021-06-22 17:33:31 -0500

    Notebook 3 is ready. All notebooks needs another review

--- programas/scala/factorial.scala
+++ programas/scala/factorial.scala
@@ -1,25 +1,48 @@
-// Scala 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import scala.annotation.tailrec
 
-// Factorial recursivo utilizando un If
-def factorialIf(n:Int):Int = if (n == 0) 1 else n * factorialIf(n - 1)
-
-def factorialCase(n:Int):Int = n match {
+def facIf(n:Int):Int = if (n == 0) 1 else n * facIf(n - 1)
+def facCase(n:Int):Int = n match {
   case 0 => 1
-  case n => n * factorialCase(n - 1)
+  case n => n * facCase(n - 1)
 }
-
-def factorialCola(n:Int):Int = {
+def facCola(n:Int):Int = {
   @tailrec
-  def iFactorialCola(n:Int, prod:Int):Int = n match {
+  def iFacCola(n:Int, prod:Int):Int = n match {
     case 0 => prod
-    case n => iFactorialCola(n - 1, n * prod)
+    case n => iFacCola(n - 1, n * prod)
   }
-  iFactorialCola(n, 1)
+  iFacCola(n, 1)
 }
 
-def tiempoEjeFac(fac:Int => Int, n:Int):Long = {
-  val tInicial = System.nanoTime
+def obtTmpEjec(fac:Int => Int, n:Int):Long = {
+  val tInicio = System.nanoTime
   fac(n)
-  System.nanoTime - tInicial
+  System.nanoTime - tInicio
 }
+
+def obtTmpEjec2(fac:Int => Int):Int => Long =
+  (n:Int) => obtTmpEjec(fac, n)
